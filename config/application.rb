7fe9cc22b0405756328c05ec635e322db750591e
matu <sm1044mt@gmail.com>
2017-12-18 05:50:56 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative 'boot'
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+module FilmArchive
+  class Application < Rails::Application
+    config.load_defaults 5.1
+    # config.time_zone = 'Tokyo'
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}'.to_s)]
+    # config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.helper false
+      g.assets false
+    end
+  end
+end
+
